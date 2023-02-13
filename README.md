7fb1edbfb48c007b6a1ca8197a4d5daeadfde447
Ilkka Kuosmanen <ilkka.kuosmanen@lightningchart.com>
2023-02-13 13:46:58 +0200

    LCJS 4.0.1

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # JavaScript Ultra Precision Trading Chart
 
-![JavaScript Ultra Precision Trading Chart](ultraPrecisionTradingChart.png)
+![JavaScript Ultra Precision Trading Chart](ultraPrecisionTradingChart-darkGold.png)
 
 This demo application belongs to the set of examples for LightningChart JS, data visualization library for JavaScript.
 
@@ -8,20 +8,20 @@ LightningChart JS is entirely GPU accelerated and performance optimized charting
 
 The demo can be used as an example or a seed project. Local execution requires the following steps:
 
-- Make sure that relevant version of [Node.js](https://nodejs.org/en/download/) is installed
-- Open the project folder in a terminal:
+-   Make sure that relevant version of [Node.js](https://nodejs.org/en/download/) is installed
+-   Open the project folder in a terminal:
 
-        npm install              # fetches dependencies
-        npm start                # builds an application and starts the development server
+          npm install              # fetches dependencies
+          npm start                # builds an application and starts the development server
 
-- The application is available at *http://localhost:8080* in your browser, webpack-dev-server provides hot reload functionality.
+-   The application is available at _http://localhost:8080_ in your browser, webpack-dev-server provides hot reload functionality.
 
 
 ## Description
 
 This example showcases how LightningChart JS can be used to enable next-generation Fintech chart applications.
 
-As proven in our [2021 line charts performance comparison](https://www.arction.com/javascript-charts-performance-comparison/), LightningChart JS line charts are **over 700 times faster** than other web charts. LightningChart JS has been proved to be able to visualize data sets with even up to 500 million data points.
+As proven in our [2021 line charts performance comparison](https://lightningchart.com/javascript-charts-performance-comparison/), LightningChart JS line charts are **over 700 times faster** than other web charts. LightningChart JS has been proved to be able to visualize data sets with even up to 500 million data points.
 
 This large data plotting capacity paired together with great zoom capabilities enables some previously unseen possibilities in Fintech applications.
 
@@ -34,13 +34,13 @@ As you can see, the data amount scales very quickly to crazy amounts. For this r
 
 To learn more about LightningChart JS and its Fintech capabilities, here are some potentially interesting links:
 
-- [Next Generation Real-Time Trading Dashboard](https://www.arction.com/lightningchart-js-interactive-examples/examples/lcjs-example-0509-dashboardRealtimeTrading.html)
+-   [Next Generation Real-Time Trading Dashboard](https://lightningchart.com/lightningchart-js-interactive-examples/examples/lcjs-example-0509-dashboardRealtimeTrading.html)
 
-- [LC JS Trading + Indicators (Volume, RSI, SMA, EMA) Showcase with real trading data from alphavantage.co](https://arction.github.io/lcjs-showcase-trading/)
+-   [LC JS Trading + Indicators (Volume, RSI, SMA, EMA) Showcase with real trading data from alphavantage.co](https://arction.github.io/lcjs-showcase-trading/)
 
-- [Candlestick, Area, DateTime features and more](https://www.arction.com/lightningchart-js-interactive-examples/examples/lcjs-example-0502-dashboardTrading.html)
+-   [Candlestick, Area, DateTime features and more](https://lightningchart.com/lightningchart-js-interactive-examples/examples/lcjs-example-0502-dashboardTrading.html)
 
-- [Latest news on LC JS performance](https://www.arction.com/high-performance-javascript-charts/)
+-   [Latest news on LC JS performance](https://lightningchart.com/high-performance-javascript-charts/)
 
 
 ## API Links
@@ -61,31 +61,31 @@ To learn more about LightningChart JS and its Fintech capabilities, here are som
 
 If you notice an error in the example code, please open an issue on [GitHub][0] repository of the entire example.
 
-Official [API documentation][1] can be found on [Arction][2] website.
+Official [API documentation][1] can be found on [LightningChart][2] website.
 
 If the docs and other materials do not solve your problem as well as implementation help is needed, ask on [StackOverflow][3] (tagged lightningchart).
 
-If you think you found a bug in the LightningChart JavaScript library, please contact support@arction.com.
+If you think you found a bug in the LightningChart JavaScript library, please contact support@lightningchart.com.
 
-Direct developer email support can be purchased through a [Support Plan][4] or by contacting sales@arction.com.
+Direct developer email support can be purchased through a [Support Plan][4] or by contacting sales@lightningchart.com.
 
 [0]: https://github.com/Arction/
-[1]: https://www.arction.com/lightningchart-js-api-documentation/
-[2]: https://www.arction.com
+[1]: https://lightningchart.com/lightningchart-js-api-documentation/
+[2]: https://lightningchart.com
 [3]: https://stackoverflow.com/questions/tagged/lightningchart
-[4]: https://www.arction.com/support-services/
+[4]: https://lightningchart.com/support-services/
 
-© Arction Ltd 2009-2020. All rights reserved.
+© LightningChart Ltd 2009-2022. All rights reserved.
 
 
-[Dashboard]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/classes/dashboard.html
-[XY cartesian chart]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/classes/chartxy.html
-[Line series]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/classes/lineseries.html
-[Axis]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/classes/axis.html
-[Axis tick strategies]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/globals.html#axistickstrategies
-[UI position origins]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/globals.html#uiorigins
-[UI layout builders]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/globals.html#uilayoutbuilders
-[Time formatting functions]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/globals.html#timeformattingfunctions
-[Synchronize axis intervals utility]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/globals.html#synchronizeaxisintervals
-[Translate point utility]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/globals.html#translatepoint
+[Dashboard]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/classes/Dashboard.html
+[XY cartesian chart]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/classes/ChartXY.html
+[Line series]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/classes/LineSeries.html
+[Axis]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/classes/Axis.html
+[Axis tick strategies]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/variables/AxisTickStrategies.html
+[UI position origins]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/variables/UIOrigins.html
+[UI layout builders]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/variables/UILayoutBuilders.html
+[Time formatting functions]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/variables/TimeFormattingFunctions.html
+[Synchronize axis intervals utility]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/functions/synchronizeAxisIntervals.html
+[Translate point utility]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/functions/translatePoint.html
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
-// Trading line chart with 1 microsecond precision data
-// 3 series, display a 2.5 second interval, total data points = 7.5 million
-
+/*
+ * Trading line chart with 1 microsecond precision data
+ * 3 series, display a 2.5 second interval, total data points = 7.5 million
+ */
+// Import LightningChartJS
 const lcjs = require('@arction/lcjs')
+
+// Import xydata
 const xydata = require('@arction/xydata')
+
 const {
     AxisTickStrategies,
     emptyFill,
@@ -14,11 +19,9 @@ const {
     UIElementBuilders,
     UILayoutBuilders,
     AutoCursorModes,
-    Themes
+    Themes,
 } = lcjs
-const {
-    createProgressiveTraceGenerator
-} = xydata
+const { createProgressiveTraceGenerator } = xydata
 
 const CHANNELS = 3
 const DATA_PER_CHANNEL = 2.5 * 1000 * 1000
@@ -27,7 +30,6 @@ const STEP_X = 10 ** -3
 const dashboard = lightningChart().Dashboard({
     numberOfColumns: 1,
     numberOfRows: CHANNELS,
-    disableAnimations: true,
     // theme: Themes.darkGold
 })
 
@@ -45,17 +47,23 @@ const chartList = new Array(CHANNELS).fill(0).map((_, i) => {
         chart.setTitle(`3 stock price trends with 1 microsecond data resolution (total ${DATA_PER_CHANNEL * CHANNELS} values)`)
     }
 
-    const axisX = chart.getDefaultAxisX().setThickness({ min: 30 }).setTickStrategy(AxisTickStrategies.Time)
+    const axisX = chart.getDefaultAxisX().setThickness({ min: 30 }).setTickStrategy(AxisTickStrategies.Time).setScrollStrategy(undefined)
     const axisY = chart.getDefaultAxisY().setTitle('Price (€)').setThickness({ min: 80 })
 
     const uiLayout = chart
-        .addUIElement(UILayoutBuilders.Column, { x: chart.getDefaultAxisX(), y: chart.getDefaultAxisY() })
+        .addUIElement(UILayoutBuilders.Column, {
+            x: chart.getDefaultAxisX(),
+            y: chart.getDefaultAxisY(),
+        })
         .setOrigin(UIOrigins.LeftTop)
-        .setPosition({ x: chart.getDefaultAxisX().getInterval().start, y: chart.getDefaultAxisY().getInterval().end })
+        .setPosition({
+            x: chart.getDefaultAxisX().getInterval().start,
+            y: chart.getDefaultAxisY().getInterval().end,
+        })
         .setMouseInteractions(false)
         .setBackground((background) => background.setStrokeStyle(emptyLine))
-    chart.getDefaultAxisX().onScaleChange((start, end) => uiLayout.setPosition({ x: start, y: chart.getDefaultAxisY().getInterval().end }))
-    chart.getDefaultAxisY().onScaleChange((start, end) => uiLayout.setPosition({ x: chart.getDefaultAxisX().getInterval().start, y: end }))
+    chart.getDefaultAxisX().onIntervalChange((_, start, end) => uiLayout.setPosition({ x: start, y: chart.getDefaultAxisY().getInterval().end }))
+    chart.getDefaultAxisY().onIntervalChange((_, start, end) => uiLayout.setPosition({ x: chart.getDefaultAxisX().getInterval().start, y: end }))
     uiLayout
         .addElement(UIElementBuilders.TextBox)
         .setText('< Stock name >')
@@ -102,6 +110,7 @@ Promise.all(
 ).then((dataSets) => {
     seriesList.forEach((series, i) => {
         series.add(dataSets[i])
+        series.axisX.fit(false)
     })
 
     // Customize chart interactions.
@@ -111,7 +120,7 @@ Promise.all(
     })
 
     // Create custom chart interaction for mouse dragging inside chart area.
-    const xBandList = chartList.map((chart) => chart.getDefaultAxisX().addBand().dispose())
+    const xBandList = chartList.map((chart) => chart.getDefaultAxisX().addBand().setVisible(false))
     chartList.forEach((chart) => {
         const axisX = chart.getDefaultAxisX()
         const axisY = chart.getDefaultAxisY()
@@ -132,18 +141,18 @@ Promise.all(
                     { x: axisX, y: axisY },
                 ).x
                 if (Math.abs(event.clientX - startLocation.x) > 10) {
-                    band.restore().setValueStart(xAxisLocationStart).setValueEnd(xAxisLocationNow)
+                    band.setVisible(true).setValueStart(xAxisLocationStart).setValueEnd(xAxisLocationNow)
                 } else {
-                    band.dispose()
+                    band.setVisible(false)
                 }
             })
         })
         chart.onSeriesBackgroundMouseDragStop((_, event, button, startLocation) => {
-            if (button !== 0 || xBandList[0].isDisposed()) return
+            if (button !== 0 || !xBandList[0].getVisible()) return
 
             const xStart = Math.min(xBandList[0].getValueStart(), xBandList[0].getValueEnd())
             const xEnd = Math.max(xBandList[0].getValueStart(), xBandList[0].getValueEnd())
-            chartList[0].getDefaultAxisX().setInterval(xStart, xEnd, false, true)
+            chartList[0].getDefaultAxisX().setInterval({ start: xStart, end: xEnd })
             xBandList.forEach((band, i) => {
                 const nChart = chartList[i]
                 let yMin = 999999
@@ -155,8 +164,8 @@ Promise.all(
                         yMax = Math.max(yMax, dp.y)
                     }
                 }
-                nChart.getDefaultAxisY().setInterval(yMin, yMax, false, true)
-                band.dispose()
+                nChart.getDefaultAxisY().setInterval({ start: yMin, end: yMax })
+                band.setVisible(false)
             })
         })
         chart.onSeriesBackgroundMouseDoubleClick((_, event) => {
@@ -174,7 +183,7 @@ Promise.all(
     let fitActive = false
     // When X Axis interval is changed, automatically fit Y axis based on visible data.
     chartList.forEach((chart, i) => {
-        chart.getDefaultAxisX().onScaleChange((xStart, xEnd) => {
+        chart.getDefaultAxisX().onIntervalChange((_, xStart, xEnd) => {
             if (fitActive) return
 
             let yMin = 999999
@@ -187,46 +196,49 @@ Promise.all(
                 }
             }
             if (yMin < 999999) {
-                chart.getDefaultAxisY().setInterval(yMin, yMax, false, true)
+                chart.getDefaultAxisY().setInterval({ start: yMin, end: yMax })
             }
         })
     })
 })
 
 // Setup custom data cursor.
-const resultTable = dashboard.addUIElement(UILayoutBuilders.Column, dashboard.engine.scale)
+const xTicks = chartList.map((chart) => chart.getDefaultAxisX().addCustomTick(UIElementBuilders.PointableTextBox).setVisible(false))
+const resultTable = dashboard
+    .addUIElement(UILayoutBuilders.Column, dashboard.engine.scale)
     .setMouseInteractions(false)
     .setOrigin(UIOrigins.LeftBottom)
     .setMargin(5)
-const resultTableRows = new Array(1 + CHANNELS).fill(0).map(_ => resultTable.addElement(UIElementBuilders.TextBox))
-resultTable.dispose()
-const xTicks = chartList.map((chart) => chart.getDefaultAxisX().addCustomTick().dispose())
+const resultTableRows = new Array(1 + CHANNELS).fill(0).map((_) => resultTable.addElement(UIElementBuilders.TextBox))
+resultTable.setVisible(false)
 
 chartList.forEach((chart) => {
     chart.setAutoCursorMode(AutoCursorModes.disabled)
-    chart.onSeriesBackgroundMouseMove((_, event) => {
+    const showCursorAtEvent = (event) => {
         const mouseLocationEngine = chart.engine.clientLocation2Engine(event.clientX, event.clientY)
-        const mouseLocationAxisX = translatePoint(mouseLocationEngine, chart.engine.scale, { x: chart.getDefaultAxisX(), y: chart.getDefaultAxisY() }).x
+        const mouseLocationAxisX = translatePoint(mouseLocationEngine, chart.engine.scale, {
+            x: chart.getDefaultAxisX(),
+            y: chart.getDefaultAxisY(),
+        }).x
         resultTableRows[0].setText(chart.getDefaultAxisX().formatValue(mouseLocationAxisX))
         for (let i = 0; i < CHANNELS; i += 1) {
             const series = seriesList[i]
             const nearestDataPoint = series.solveNearestFromScreen(mouseLocationEngine)
-            resultTableRows[1 + i].setText(series.getName() + ': ' + (nearestDataPoint ? chart.getDefaultAxisY().formatValue(nearestDataPoint.location.y) + ' €' : ''))
+            resultTableRows[1 + i].setText(
+                series.getName() + ': ' + (nearestDataPoint ? chart.getDefaultAxisY().formatValue(nearestDataPoint.location.y) + ' €' : ''),
+            )
         }
-        resultTable
-            .restore()
-            .setPosition(mouseLocationEngine)
-        xTicks.forEach((xTick) => xTick
-            .restore()
-            .setValue(mouseLocationAxisX)
-        )
-    })
+        resultTable.setVisible(true).setPosition(mouseLocationEngine)
+        xTicks.forEach((xTick) => xTick.setVisible(true).setValue(mouseLocationAxisX))
+    }
+    chart.onSeriesBackgroundMouseMove((_, event) => showCursorAtEvent(event))
+    chart.getSeries().forEach(series => series.onMouseMove((_, event) => showCursorAtEvent(event)))
     chart.onSeriesBackgroundMouseDragStart(() => {
-        resultTable.dispose()
-        xTicks.forEach((xTick) => xTick.dispose())
+        resultTable.setVisible(false)
+        xTicks.forEach((xTick) => xTick.setVisible(false))
     })
     chart.onSeriesBackgroundMouseLeave(() => {
-        resultTable.dispose()
-        xTicks.forEach((xTick) => xTick.dispose())
+        resultTable.setVisible(false)
+        xTicks.forEach((xTick) => xTick.setVisible(false))
     })
 })
